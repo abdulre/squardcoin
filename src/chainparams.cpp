16880e35a31ee7fafa335cc8c6724796c760d799
abdulre <abdulre8@gmail.com>
2025-09-26 21:47:12 +0100

    Update chainparams.cpp
    Update seed nodes to squardcoin.com DNS

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -277,8 +277,9 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("172.235.197.90");
-        vSeeds.emplace_back("172.233.88.77");
+        vSeeds.clear();
+        vSeeds.emplace_back("seed1.squardcoin.com");
+        vSeeds.emplace_back("seed2.squardcoin.com");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,63);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,64);
